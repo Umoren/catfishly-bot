389b05e27db6a171ec7d02a4979939fb5d5eeb99
Samuel <samuelumoren365@gmail.com>
2023-04-16 23:42:28 +0100

    embedded the chatbot

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { Fragment } from 'react';
 import Head from 'next/head';
 // import { FaQuestionCircle } from 'react-icons/fa';
 import { Accordion, AccordionItem } from "flowbite-react";
+import Image from 'next/image';
 
 export default function ChatbotPage() {
   const currentYear = new Date().getFullYear();
@@ -22,15 +23,24 @@ export default function ChatbotPage() {
       <Head>
         <title>Catfishly - Your Catfish Farming Assistant</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        {/* <script id="cgpt-script-chunky" data-handle="catfishly01" src="https://app.getchunky.io/app/chatUI-web/loader.js"></script> */}
       </Head>
       <div className="flex flex-col h-screen">
         {/* Header */}
-        <header className="bg-white py-8 px-4">
+        <header className="bg-white py-8 px-4 md:flex gap-3 justify-center items-center">
+
+          <Image
+            src={'/catfishly.png'}
+            width={50}
+            height={50}
+            className='mx-auto md:mx-0'
+          />
           <h1 className="text-3xl font-bold text-center text-aquamarine-500">I&apos;m Catfishly, your catfish farming assistant. Ask me anything.</h1>
+
         </header>
 
         {/* Main content */}
-        <main className="flex-grow flex items-center justify-center">
+        <main className="flex-grow flex items-center justify-center p-8">
           <div className="w-full md:w-1/2 lg:w-1/3">
             {/* FAQ section */}
             <Accordion alwaysOpen={true}>
@@ -52,6 +62,8 @@ export default function ChatbotPage() {
         <footer className="bg-black py-4 px-4 text-white text-center">
           <p className="text-sm">Catfishly@{currentYear}</p>
         </footer>
+
+        <script id="cgpt-script-chunky" data-handle="catfishly01" src="https://app.getchunky.io/app/chatUI-web/loader.js"></script>
       </div>
     </Fragment>
   );
